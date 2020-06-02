a3304a087e40ed411bef588d3d57053000356dfe
maximilliancharlemagne <maxwell.k.roberts@gmail.com>
2020-06-02 09:42:43 -0400

    added semicolons

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-require('dotenv').config()
+require('dotenv').config();
 
 //basic test server for building profile page
 
-const express = require('express')
-const exphbs = require('express-handlebars')
-const path = require('path')
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
