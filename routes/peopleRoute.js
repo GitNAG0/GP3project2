832b7a2f1291e9d97d8ca065159cad9c10a25879
Kasey Agapinan <agapinank@gmail.com>
2020-06-07 14:34:33 -0700

    People and Round Routes
    Signed-off-by: Kasey Agapinan <agapinank@gmail.com>

--- routes/peopleRoute.js
+++ routes/peopleRoute.js
@@ -2,45 +2,45 @@ const app = require('express').Router();
 const db = require('../models');
 ///getOneCompanyPeople/:company_id
 module.exports = function (app) {
-  app.get('/OneCompanyPeople/:company_id', function (req, res) {
-    db.OneCompanyPeople.findOne({
+  app.get('/getOneCompanyPeople/:company_id', function (req, res) {
+    db.getOneCompanyPeople.findOne({
       where: {
         id: req.params.company_id,
       },
-    }).then(function (dbOneCompanyPeople) {
-      res.json(dbOneCompanyPeople);
+    }).then(function (dbgetOneCompanyPeople) {
+      res.json(dbgetOneCompanyPeople);
     });
   });
   //Get onePerson By ID
-  app.get('/OnePerson/:id', function (req, res) {
-    db.OnePerson.findOne({
+  app.get('/getOnePerson/:id', function (req, res) {
+    db.getOnePerson.findOne({
       where: {
         id: req.params.users_id,
       },
-    }).then(function (dbOnePerson) {
-      res.json(dbOnePerson);
+    }).then(function (dbgetOnePerson) {
+      res.json(dbgetOnePerson);
     });
   });
   //PUT update to person Profile
-  app.put('/OnePerson/:id', function (req, res) {
-    db.OnePerson.update({
+  app.put('/updateOnePerson/:id', function (req, res) {
+    db.updateOnePerson.update({
       where: {
         id: req.params.users_id,
       },
-    }).then(function (dbOnePerson) {
-      res.json(dbOnePerson);
+    }).then(function (dbgetOnePerson) {
+      res.json(dbgetOnePerson);
     });
   });
   //POSt CreateOne Person
-  app.post('/onePerson', function (req, res) {
+  app.post('/createOnePerson', function (req, res) {
     console.log(req.body);
-    db.OnePerson.create(req.body).then(function (dbOnePerson) {
+    db.createOnePerson.create(req.body).then(function (dbOnePerson) {
       res.json(dbOnePerson);
     });
   });
   //Delete One Person By ID
-  app.delete('onePerson/:id', function (req, res) {
-    db.OnePerson.destroy({
+  app.delete('deleteOnePerson/:id', function (req, res) {
+    db.deleteOnePerson.destroy({
       where: { id: req.params.users_id },
     }).then(function (dbOnePerson) {
       res.json(dbOnePerson);
---
+++ routes/roundRoute.js
@@ -0,0 +1,69 @@
+const app = require('express').Router();
+const db = require('../models');
+
+module.exports = function (app) {
+  app.get('/getLastRound/:company_id', function (req, res) {
+    db.getLastRound
+      .findOne({
+        where: {
+          id: req.params.company_id,
+        },
+      })
+      .then(function (dbgetLastRound) {
+        res.json(getLastRound);
+      });
+  });
+
+  app.get('/getOneCompanyRounds/:company_id', function (req, res) {
+    db.getOneCompanyRounds
+      .findOne({
+        where: {
+          id: req.params.company_id,
+        },
+      })
+      .then(function (dbgetOneCompanyRounds) {
+        res.json(dbgetOneCompanyRounds);
+      });
+  });
+
+  app.get('/getOneRound/:id', function (req, res) {
+    db.getOneCompanyRounds
+      .findOne({
+        where: {
+          id: req.params.id,
+        },
+      })
+      .then(function (dbgetOneCompanyRounds) {
+        res.json(dbgetOneCompanyRounds);
+      });
+  });
+
+   app.put('/updateOneRound', function (req, res) {
+     db.updateOneRound
+       .update({
+         where: {
+           id: req.params.company_id,
+         },
+       })
+       .then(function (dbgetOneCompanyRounds) {
+         res.json(dbgetOneCompanyRounds);
+       });
+   });
+
+   app.post('/createOneRound', function (req, res) {
+     console.log(req.body);
+     db.createOneRound.create(req.body).then(function (dbgetOneCompanyRounds) {
+       res.json(dbgetOneCompanyRounds);
+     });
+   });
+
+    app.delete('deleteOneRound/:id', function (req, res) {
+    db.deleteOneRound.destroy({
+      where: { id: req.params.company_id },
+    }).then(function (dbgetOneCompanyRounds) {
+      res.json(dbgetOneCompanyRounds);
+    });
+  });
+};
+
+};
